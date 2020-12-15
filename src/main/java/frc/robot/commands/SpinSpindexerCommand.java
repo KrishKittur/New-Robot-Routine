3fe2c00c3797f241bd3ec436d9b4fe3f273d3e4d
KrishKittur <kkittur24@dtechhs.org>
2020-12-14 16:51:05 -0800

    Added the spin command for the spindexer subsystem

---
+++ src/main/java/frc/robot/commands/SpinSpindexerCommand.java
@@ -0,0 +1,37 @@
+package frc.robot.commands;
+
+import frc.robot.subsystems.SpindexerSubsystem;
+import edu.wpi.first.wpilibj.util.Units;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class SpinSpindexerCommand extends CommandBase {
+    
+    SpindexerSubsystem req_subsystem;
+    double startDistance;
+
+    public SpinSpindexerCommand(SpindexerSubsystem subsystem) {
+        req_subsystem = subsystem;
+        
+        addRequirements(subsystem);
+    }
+
+    // On the initialization set the starting distance and spin up the spindexer motor
+    @Override
+    public void initialize() {
+        startDistance = req_subsystem.readSpindexerEncoder();
+        req_subsystem.setSpindexerMotor(0.6);
+    }
+
+    // When the command finishes turn the spindexer motor off
+    @Override
+    public void end(boolean interrupted) {
+        req_subsystem.setSpindexerMotor(0);
+    }
+
+    // Returns true when the spindexer has completed a full rotation
+    @Override
+    public boolean isFinished() {
+        return req_subsystem.readSpindexerEncoder() > startDistance + Units.degreesToRadians(360);
+    }
+     
+}
