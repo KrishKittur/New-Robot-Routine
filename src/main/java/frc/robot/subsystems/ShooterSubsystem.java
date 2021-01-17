ad868fc598415d675068c25a9276b5662b763066
Krish Kittur <kkittur24@dtechhs.org>
2021-01-17 11:08:18 -0800

    Smol change

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -4,6 +4,7 @@ import static frc.robot.Constants.Shooter.*;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 
@@ -35,5 +36,11 @@ public class ShooterSubsystem extends SubsystemBase {
    // Method to read the encoders values
    public double readShooterEncoder() {
         return shooterEncoder.getRate() * 60;
+   }
+
+   // In the periodic method, add the encoders readings to Smart Dashboard
+   @Override
+   public void periodic() {
+        SmartDashboard.putNumber("Shooter Speed", readShooterEncoder());
    }
 }
